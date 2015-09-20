a17ec56b44fe0f55281f630c5f9626bbefbadeaf
Yuji Tanimoto <y.tanimoto13@gmail.com>
2015-09-21 02:30:08 +0900

    Add image model

---
+++ spec/models/image_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Image, type: :model do
+  describe "validation" do
+    describe "url" do
+      let(:no_url_image) { Image.new(url: nil, title: "title") }
+
+      it "require url" do
+        expect(no_url_image).to_not be_valid
+        expect(no_url_image.errors[:url]).to_not be_empty
+      end
+    end
+
+    describe "title" do
+      let(:no_title_image) { Image.new(url: "hoge", title: nil) }
+
+      it "require title" do
+        expect(no_title_image).to_not be_valid
+        expect(no_title_image.errors[:title]).to_not be_empty
+      end
+    end
+  end
+end
